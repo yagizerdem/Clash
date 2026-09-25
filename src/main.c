6c28f84f80e327741c16b4085082231cc3271afd
yagiz erdem <2yagiz004@gmail.com>
2026-09-25 15:43:08 +0300

    update: tree sitter library setup

--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
 #include "stack-trace/crash_handler.h"
 #include  <stdio.h>
+#include "tree-sitter-bash/src/tree_sitter/parser.h"
+
+const TSLanguage *tree_sitter_bash(void);
 
 int main(void) {
-     clash_install_crash_handler();
+    clash_install_crash_handler();
     printf("test");
-    int *x = NULL;
-    *x = 10;
+
+
+
+
     return 0;
 }
