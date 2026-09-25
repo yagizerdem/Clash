b65da992c48ac5f0c7d4205794f2cf6d5c3a4c7f
yagiz erdem <2yagiz004@gmail.com>
2026-09-25 13:46:16 +0300

    update: agnets-md file added

--- main.c
+++ main.c
@@ -9,25 +9,6 @@ int main(void) {
 
 
 
-    pid_t pid = fork();
-    if (pid == -1) {
-        perror("fork");
-    }
-    else if (pid == 0) {
-
-        char *arg[6] = {"/bin/bash", "-c", "echo yagiz erdem", NULL} ;
-        char *env[] = { NULL };
-
-        execve(arg[0], arg, env);
-        perror("execve");   /* execve() returns only on error */
-        exit(EXIT_FAILURE);
-
-    }
-    else {
-        printf("parent %d \n", pid);
-        wait(NULL);
-    }
-
 
     return 0;
 };
